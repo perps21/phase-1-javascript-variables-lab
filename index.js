2e77d267f53cc8c0c2ee7219ca498a0d6a32accf
perps21 <wise.abonyo@gmail.com>
2023-06-13 08:18:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// let companyName = "Scruber";
+
+const companyName = "Scuber"
+
+// const mostProfitableNeighborhood = "chelsea"
+
+ let mostProfitableNeighborhood = "Chelsea"
+
+// const companyCeo = "Susan Smith"
+
+let companyCeo = "Susan Smith"
